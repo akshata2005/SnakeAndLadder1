cf9b8eb952b737cffaf6af1ef1fd94676f934210
akshata2005 <akshatasawant1805@gmail.com>
2022-03-09 07:03:27 +0530

    [Akshata] Add: Start Position os snake

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Showing Welcome message on console
+            Console.WriteLine("Welcome to Snake And Ladder Game Program\n");
+            //Object creation of class. 
+            StartPosition startPosition = new StartPosition();
+            startPosition.RunGame();     //call method of class.
+            Console.ReadLine();
+        }
+    }
+}
